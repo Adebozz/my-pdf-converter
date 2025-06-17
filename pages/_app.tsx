b60f2c1f5a6d930823c6b4c4b809549076f2003e
Adeboss <ademolaadeigbe3@gmail.com>
2025-06-17 22:16:12 +0100

    `updated the project`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,25 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+'use client'
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import './globals.css'
+
+const config = {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+const theme = extendTheme({ config })
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+  return (
+    <html lang="en">
+      <head />
+      <body>
+        {/* This ensures Chakra reads the stored color mode */}
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <ChakraProvider theme={theme}>{children}</ChakraProvider>
+      </body>
+    </html>
+  )
 }
